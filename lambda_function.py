b8328df437c709276c67238e344f12ada9d090c3
Mohammad Rahman <mcmogaming@gmail.com>
2023-05-05 12:10:13 -0400

    modified

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,14 @@
 import json
+import boto3
 
 def lambda_handler(event, context):
-    # TODO implement
+    
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('todoTable')
+    
+    response = table.put_item(
+        Item={'sample':'test'})
+            
     return {
         'statusCode': 200,
         'body': json.dumps('This message has been modified!')
