f9c56615df028f4c8bfe0b33f5ba6b643120ee0b
Mohammad Rahman <mcmogaming@gmail.com>
2023-05-05 12:19:45 -0400

    modified

--- lambda_function.py
+++ lambda_function.py
@@ -6,10 +6,20 @@ def lambda_handler(event, context):
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table('todoTable')
     
+    http_method = event['requestContext']['http']['method']
+    
+    #Add an item to db
+    
+    #Delete an item from db
+    
+    #List all items from db
+    
+    
     response = table.put_item(
         Item={'id':'123','sample':'test'})
             
     return {
         'statusCode': 200,
-        'body': event
+        'body': event,
+        'method': http_method
     }
